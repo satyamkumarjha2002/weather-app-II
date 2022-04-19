11f483856966947be7ee564f9ef9cfe605a93a70
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-04-19 23:40:04 +0530

    movie details show feature added

--- index.js
+++ index.js
@@ -16,15 +16,18 @@ function append(data) {
     sugesation.innerHTML = null
     for (let i = 0; i < data.length; ++i) {
         data.forEach(element => {
+            let div=document.createElement("div");
             let movie_name = document.createElement("p");
             movie_name.innerText = element.Title;
-            console.log(element.Title);
-            sugesation.append(movie_name);
+            div.append(movie_name);
+            div.addEventListener("click",function(){
+                showDetails(element);
+            })
+            sugesation.append(div);
         });
     }
 
 }
-
 async function main() {
     let data = await search_movies();
         if (data != undefined) {
@@ -37,4 +40,23 @@ function debauncing(func,delay){
     id=setTimeout(function(){
         func();
     },delay);
+}
+function showDetails(element){
+    event.preventDefault();
+    let show_movie=document.querySelector("#show_movie");
+    show_movie.innerHTML=null;
+    let div1=document.createElement("div")
+    let image=document.createElement("img");
+    image.src=element.Poster;
+    div1.append(image);
+    let div2=document.createElement("div");
+    let name=document.createElement("h3");
+    name.innerText=element.Title;
+    let realese_year=document.createElement("p");
+    realese_year.innerText=element.Year;
+    let type=document.createElement("p");
+    type.innerText=element.Type;
+    div2.append(name,realese_year,type);
+    show_movie.append(div1,div2);
+    console.log(element);
 }
